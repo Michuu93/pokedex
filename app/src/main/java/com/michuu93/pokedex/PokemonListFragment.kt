137e5558f79a6b07d4ce848ae0ea565dd6c68caa
Michał H <michuu93@gmail.com>
2019-05-11 22:45:20 +0200

    ArrayAdapter initial loads 100 pokemon names

--- app/src/main/java/com/michuu93/pokedex/PokemonListFragment.kt
+++ app/src/main/java/com/michuu93/pokedex/PokemonListFragment.kt
@@ -3,54 +3,50 @@ package com.michuu93.pokedex
 import android.os.Bundle
 import android.support.v4.app.ListFragment
 import android.util.Log
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
+import kotlin.collections.ArrayList
 
 class PokemonListFragment : ListFragment() {
-    private lateinit var pokemons: List<String>
+    private var pokemons: ArrayList<String> = arrayListOf()
     private lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         retainInstance = true
 
-        pokemons = listOf(
-            "TEST",
-            "LIST"
-        )
-
-        loadPokemons()
-
         context.let {
             adapter = ArrayAdapter(it!!, android.R.layout.simple_list_item_1, pokemons)
         }
 
+        loadPokemons()
     }
 
-    fun loadPokemons() {
-        PokemonApiClient().getPokemons(10).enqueue(
+    override fun onListItemClick(l: ListView?, v: View?, position: Int, id: Long) {
+        Log.d("onListItemClick", position.toString() + " - " + pokemons[position])
+    }
+
+    private fun loadPokemons() {
+        PokemonApiClient().getPokemons(100).enqueue(
             object : ApolloCall.Callback<Pokemons.Data>() {
                 override fun onResponse(response: Response<Pokemons.Data>) {
                     val pokemons = response.data()?.pokemons?.map { pokemon ->
                         pokemon.fragments().pokemonSimple
                     }!!
-
-                    Log.d("POKEMONS", pokemons.toString())
-
                     activity!!.runOnUiThread {
                         val pokemonsOnlyNames = pokemons.map { pokemonSimple -> pokemonSimple.name() }.requireNoNulls().toList()
-                        this@PokemonListFragment.pokemons = pokemonsOnlyNames
-                        adapter.notifyDataSetChanged()
-                        adapter.notifyDataSetInvalidated()
+                        adapter.clear()
+                        adapter.addAll(pokemonsOnlyNames)
                         Log.d("POKEMONS UPDATED", pokemonsOnlyNames.toString())
                     }
                 }
 
                 override fun onFailure(e: ApolloException) {
-                    //TODO throw error
+                    Log.e("POKEMON API", e.toString())
                 }
             })
     }
